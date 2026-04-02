21140b0bb0d14864e6f0f23b50f5dc27a717b7e1
Gustavo de Souza <gudesouza.sz@gmail.com>
2026-04-02 14:21:08 -0300

    pagina cadastro criada

---
+++ src/app/pages/cadastro/cadastro.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro',
+  imports: [],
+  template: ` <p>cadastro works!</p> `,
+  styles: ``,
+})
+export class Cadastro {}
