cf33e696eaf10fcff4b0b6bcd5e6a7ba0e8150ef
Gustavo de Souza <gudesouza.sz@gmail.com>
2026-06-09 17:53:13 -0300

    routas servicos e financeiro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,9 @@ export const routes: Routes = [
 {path: 'painel',loadComponent: () => import('./core/painel-layout/painel-layout').then(m => m.PainelLayout),
     children: [
       {path: 'dashboard', loadComponent: () => import('./pages/dashboard-tecnico/dashboard-tecnico').then(m => m.DashboardTecnico)},
-      {path: 'agenda', loadComponent: () => import('./pages/agenda-tecnico/agenda-tecnico').then(m => m.AgendaTecnico)}
+      {path: 'agenda', loadComponent: () => import('./pages/agenda-tecnico/agenda-tecnico').then(m => m.AgendaTecnico)},
+      {path: 'servicos', loadComponent: () => import('./pages/servicos-tecnico/servicos-tecnico').then(m => m.ServicosTecnico)},
+      {path: 'financeiro', loadComponent: () => import('./pages/financeiro-tecnico/financeiro-tecnico').then(m => m.FinanceiroTecnico)},
     ]},
 
 {
