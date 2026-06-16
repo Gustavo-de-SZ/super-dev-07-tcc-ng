14d2b2d0f332c9c7e3a72a09c99049efed33040e
Gustavo de Souza <gudesouza.sz@gmail.com>
2026-06-16 17:59:21 -0300

    interface cliente

---
+++ src/app/models/cliente.ts
@@ -0,0 +1,10 @@
+export interface Cliente {
+  nome: string;
+  empresa: string;
+  avaliacao: number;
+  email: string;
+  telefone: string;
+  local: string;
+  servicosAtivos: number;
+  servicosConcluidos: number;
+}
